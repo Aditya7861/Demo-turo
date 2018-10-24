ee6c843d01828575b7fee42e65d491b4d30531f8
Aditya7861 <aditya9640@gmail.com>
2018-10-24 12:34:37 +0530

    Uploading networking files

---
+++ capitans-room.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/py-the-captains-room/problem
+k,arr = int(input()),list(map(int,input().split()))
+my_set = set(arr)
+print(((sum(my_set)*k)-sum(arr))//(k-1))
+    
+
